7d5c0fd1c2054e1c2dc449e701a7cd49732225f8
aarafrao22 <aarafrao22@gmail.com>
2021-11-21 07:37:43 +0500

    ChatBotApp

--- app/src/main/java/com/example/chatbotjava/ChatRVAdapter.java
+++ app/src/main/java/com/example/chatbotjava/ChatRVAdapter.java
@@ -1,29 +1,91 @@
 package com.example.chatbotjava;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ChatRVAdapter extends RecyclerView.Adapter {
+    private ArrayList<ChatModel> chatsModelArrayList;
+    private Context context;
+
+    public ChatRVAdapter(ArrayList<ChatModel> chatsModelArrayList, Context context) {
+        this.chatsModelArrayList = chatsModelArrayList;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        switch (viewType){
+            case '0':
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_chat,parent,false);
+                UserViewHolder userViewHolder = new UserViewHolder(view);
+                return userViewHolder;
+
+            case '1':
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_robot_chat,parent,false);
+                BotViewHolder botViewHolder = new BotViewHolder(view);
+                return botViewHolder;
+        }
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ChatModel chatModel = chatsModelArrayList.get(position);
+        switch (chatModel.getSender()){
+            case "user":
+                ((UserViewHolder)holder).UserTextView.setText(chatModel.getMessage());
+                break;
 
+            case "bot":
+                ((BotViewHolder)holder).BotTextView.setText(chatModel.getMessage());
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return chatsModelArrayList.size();
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        switch (chatsModelArrayList.get(position).getSender()){
+            case "user":
+                return 0;
+
+            case "bot":
+                return 1;
+
+            default:
+                return -1;
+        }
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder{
+        TextView UserTextView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            UserTextView = itemView.findViewById(R.id.userTV);
+        }
+    }
+
+    public static class BotViewHolder extends RecyclerView.ViewHolder{
+        TextView BotTextView;
+
+        public BotViewHolder(@NonNull View itemView) {
+            super(itemView);
+            BotTextView = itemView.findViewById(R.id.robotTV);
+        }
     }
 }
--- app/src/main/java/com/example/chatbotjava/MainActivity.java
+++ app/src/main/java/com/example/chatbotjava/MainActivity.java
@@ -1,14 +1,96 @@
 package com.example.chatbotjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private EditText edEditText;
+    private FloatingActionButton btnFloatingActionButton;
+    private final String BotKey = "bot";
+    private final String UserKey = "user";
+    private ArrayList<ChatModel> chatModelsArrayList;
+    private ChatRVAdapter chatRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        edEditText = findViewById(R.id.txtMessage);
+        btnFloatingActionButton = findViewById(R.id.btnSend);
+
+        chatModelsArrayList = new ArrayList<ChatModel>();
+        chatRVAdapter = new ChatRVAdapter(chatModelsArrayList,this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(chatRVAdapter);
+
+        btnFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edEditText.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Type Anything First", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    getResponse(edEditText.getText().toString());
+                }
+            }
+        });
+
+
     }
-}
+
+    private void getResponse(String message) {
+        chatModelsArrayList.add(new ChatModel(UserKey,message));
+        chatRVAdapter.notifyDataSetChanged();
+
+        String url = "http://api.brainshop.ai/get?bid=161229&key=V34Ew7CIMrmeSQiG&uid=[uid]&msg="+message;
+        String baseURL = "http://api.brainshop.ai";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
+        Call<MessageModel> call= retrofitAPI.getMessage(url);
+        call.enqueue(new Callback<MessageModel>() {
+            @Override
+            public void onResponse(Call<MessageModel> call, Response<MessageModel> response) {
+                MessageModel msg = response.body();
+                chatModelsArrayList.add(new ChatModel(BotKey,msg.getCnt()));
+                chatRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<MessageModel> call, Throwable t) {
+                chatModelsArrayList.add(new ChatModel(BotKey,"Plz Revert Your Message"));
+                chatRVAdapter.notifyDataSetChanged();
+            }
+        });
+
+            }
+        };
+
